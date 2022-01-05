1c2eab76817e9f3439631d06a6db3fe814ef6c01
Map937 <Matt_7_1993@hotmail.co.uk>
2022-01-05 16:19:29 +0000

    DrinkService created and implements ServiceMethods

---
+++ Spring-Drinks/src/main/java/com/qa/SpringDrinks/service/DrinkService.java
@@ -0,0 +1,42 @@
+package com.qa.SpringDrinks.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.qa.SpringDrinks.domain.Drink;
+
+@Service
+public class DrinkService implements ServiceMethods<Drink> {
+
+	@Override
+	public Drink create(Drink t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Drink> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Drink getById(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Drink update(long id, Drink t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean delete(long id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
